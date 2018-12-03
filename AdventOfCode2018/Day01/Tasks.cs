dc58bfbb9adbf85f271bc2ae05837531d9bd4be1
Janis <Fallyx@users.noreply.github.com>
2018-12-03 14:26:19 +0100

    Added Tasks of Day1

---
+++ AdventOfCode2018/Day01/Tasks.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2018.Day01
+{
+    class Tasks
+    {
+        public static void Task1()
+        {
+            int frequency = 0;
+            string line;
+
+            StreamReader sr = new StreamReader(@"Day01/Input.txt");
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line[0] == '+')
+                {
+                    frequency += int.Parse(line.Substring(1));
+                }
+                else
+                {
+                    frequency -= int.Parse(line.Substring(1));
+                }
+            }
+
+            Console.WriteLine(frequency);
+        }
+
+        public static void Task2()
+        {
+            Dictionary<int, bool> freqs = new Dictionary<int, bool>();
+            bool breakLoop = false;
+            string line;
+            int frequency = 0;
+
+            while (!breakLoop)
+            {
+                using (StreamReader reader = new StreamReader(@"Day01/Input.txt"))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line[0] == '+')
+                        {
+                            frequency += int.Parse(line.Substring(1));
+                        }
+                        else
+                        {
+                            frequency -= int.Parse(line.Substring(1));
+                        }
+
+                        if (freqs.ContainsKey(frequency))
+                        {
+                            breakLoop = true;
+                            break;
+                        }
+                        else
+                        {
+                            freqs.Add(frequency, true);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(frequency);
+        }
+    }
+}
--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2018
+﻿namespace AdventOfCode2018
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Day01.Tasks.Task1();
+            Day01.Tasks.Task2();
         }
     }
 }
